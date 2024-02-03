2262c53067c5f8b1e61ab47607490b7ce363763e
Robert Ambrose <robertambrose21@gmail.com>
2024-02-04 12:20:09 +1300

    work: add uuid generator

--- src/common/core/util/idgenerator.h
+++ src/common/core/util/idgenerator.h
@@ -2,7 +2,16 @@
 
 #include <cstdint>
 
+#include "uuid_v4/uuid_v4.h"
+#include "uuid_v4/endianness.h"
+
 inline uint32_t getNewId(void) {
     static uint32_t id = 0;
     return id++;
+}
+
+inline std::string getNewUUID(void) {
+    UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
+    UUIDv4::UUID uuid = uuidGenerator.getUUID();
+    return uuid.str();
 }
