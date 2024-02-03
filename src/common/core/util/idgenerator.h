cbf7bedd8217d226b7f9b66db6ce793b92dc95a0
Robert Ambrose <robertambrose21@gmail.com>
2024-02-04 12:43:18 +1300

    work: add wrapper for uuid_v4 so we can use the faster comparator

--- src/common/core/util/idgenerator.h
+++ src/common/core/util/idgenerator.h
@@ -10,8 +10,20 @@ inline uint32_t getNewId(void) {
     return id++;
 }
 
-inline std::string getNewUUID(void) {
+struct UUID {
+    UUIDv4::UUID id;
+
+    std::string getString(void) {
+        return id.str();
+    }
+
+    bool operator==(const UUID& other) const {
+        return id == other.id;
+    }
+};
+
+inline UUID getNewUUID(void) {
     UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
     UUIDv4::UUID uuid = uuidGenerator.getUUID();
-    return uuid.str();
+    return { uuid };
 }
